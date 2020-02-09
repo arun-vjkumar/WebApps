1f087448524d4e95cec0a84cad7d4f42d38733eb
arunvrvce@gmail.com <Arun2123$#$>
2020-02-09 21:34:37 +0530

    added constants and config files

--- WeatherAndNews/app.ts
+++ WeatherAndNews/app.ts
@@ -4,8 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+    res.send('Welcome To View Weather and News!');
 });
+
 app.listen(port, err => {
     if (err) {
         return console.error(err);
